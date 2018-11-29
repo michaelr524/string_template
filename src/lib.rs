4771e73eb891a98ed47b464a751775205ef21d71
Michael Ravits <michael@xlucidity.com>
2018-11-30 01:19:42 +0200

    render positional arguments

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub struct Template {
 
 impl Template {
     pub fn new(template: &str) -> Self {
-        let regex = Regex::new(r"\{\{([^}]+)\}\}").unwrap();
+        let regex = Regex::new(r"\{\{([^}]*)\}\}").unwrap();
 
         Template {
             src: template.to_owned(),
@@ -21,7 +21,33 @@ impl Template {
         }
     }
 
-    pub fn render(&self, m: &HashMap<&str, &str>) -> String {
+    /// ```
+    /// # Examples
+    ///
+    /// let template = Template::new("Hi, my name is {{name}} and I'm a {{lang}} developer.");
+    ///
+    /// let mut args = HashMap::new();
+    /// args.insert("name", "Michael");
+    /// args.insert("lang", "Rust");
+    /// let s = template.render(&args);
+    ///
+    /// assert_eq!(s, "Hi, my name is Michael and I'm a Rust developer.");
+    ///
+    /// let mut args1 = HashMap::new();
+    /// args1.insert("name", "Vader");
+    /// args1.insert("lang", "Dart");
+    /// let s2 = template.render(&args1);
+    ///
+    /// assert_eq!(s2, "Hi, my name is Vader and I'm a Dart developer.");
+    /// ```
+    pub fn render(&self, vals: &HashMap<&str, &str>) -> String {
+        self.render_named(vals)
+    }
+
+    ///
+    /// See render() for examples.
+    ///
+    pub fn render_named(&self, vals: &HashMap<&str, &str>) -> String {
         let mut parts: Vec<&str> = vec![];
         let template_str = &self.src;
 
@@ -52,7 +78,7 @@ impl Template {
 
             // if value passed for argument then append it, otherwise append original argument
             // name with braces
-            match m.get(arg_name) {
+            match vals.get(arg_name) {
                 Some(s) => parts.push(s),
                 _ => parts.push(arg),
             }
@@ -61,7 +87,7 @@ impl Template {
         }
 
         let template_len = template_str.len();
-        // if last arg end index isn't the end of the string the copy
+        // if last arg end index isn't the end of the string then copy
         // from last arg end till end of template string
         if let Some(last_pos) = prev_end {
             if last_pos < template_len {
@@ -71,28 +97,56 @@ impl Template {
 
         parts.join("")
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    /// ```
+    /// let template = Template::new("Hi, my name is {{}} and I'm a {{}} developer.");
+    ///
+    /// let args = vec!["Michael", "Rust"];
+    /// let s = template.render_positional(&args);
+    /// assert_eq!(s, "Hi, my name is Michael and I'm a Rust developer.");
+    ///
+    /// let args1 = vec!["Vader", "Dart"];
+    /// let s2 = template.render_positional(&args1);
+    /// assert_eq!(s2, "Hi, my name is Vader and I'm a Dart developer.");
+    /// ```
+    pub fn render_positional(&self, vals: &[&str]) -> String {
+        let mut parts: Vec<&str> = vec![];
+        let template_str = &self.src;
 
-    #[test]
-    fn it_works() {
-        let template = Template::new("Hi, my name is {{name}} and I'm a {{lang}} developer.");
+        // get index of first arg match or return a copy of the template if no args matched
+        let first = match self.matches.first() {
+            Some((start, _)) => *start,
+            _ => return template_str.clone(),
+        };
 
-        let mut args = HashMap::new();
-        args.insert("name", "Michael");
-        args.insert("lang", "Rust");
-        let s = template.render(&args);
+        // copy from template start to first arg
+        if first > 0 {
+            parts.push(&template_str[0..first])
+        }
 
-        assert_eq!(s, "Hi, my name is Michael and I'm a Rust developer.");
+        // keeps the index of the previous argument end
+        let mut prev_end: Option<usize> = None;
 
-        let mut args1 = HashMap::new();
-        args1.insert("name", "Vader");
-        args1.insert("lang", "Dart");
-        let s2 = template.render(&args1);
+        for ((start, end), val) in self.matches.iter().zip(vals.iter()) {
+            // copy from previous argument end till current argument start
+            if let Some(last_end) = prev_end {
+                parts.push(&template_str[last_end..*start])
+            }
 
-        assert_eq!(s2, "Hi, my name is Vader and I'm a Dart developer.");
+            parts.push(val);
+
+            prev_end = Some(*end);
+        }
+
+        let template_len = template_str.len();
+        // if last arg end index isn't the end of the string then copy
+        // from last arg end till end of template string
+        if let Some(last_pos) = prev_end {
+            if last_pos < template_len {
+                parts.push(&template_str[last_pos..template_len])
+            }
+        }
+
+        parts.join("")
     }
 }
